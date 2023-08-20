2df420a8483e925044a26062c03904c146af98ee
Shtepselevich <sinelnikov.romann>
2023-08-20 21:48:42 +0300

    feat: creted slider in about section

--- library/js/slider-about.js
+++ library/js/slider-about.js
@@ -1,20 +1,47 @@
-const sliderImage = document.querySelectorAll(".slider-wrapper__image"),
-  sliderLine = document.querySelectorAll(".slider-wrapper"),
-  sliderDots = document.querySelectorAll(".slider-pagination__button"),
-  sliderBtnPrev = document.querySelectorAll(".slider-button__prev"),
-  sliderBtnNext = document.querySelectorAll(".slider-button__next");
-let sliderCount = 0,
-  sliderWidth;
-// Кнопки листания слайда назад и вперед
-sliderBtnNext.addEventListener("click, nextSlide");
-sliderBtnPrev.addEventListener("click, prevSlide");
-// Перелистывает слайд вперёд
-function nextSlide() {
-  sliderCount++;
-  if (sliderCount >= sliderImages.lenght) sliderCount = 0;
-}
-// Перелистывает слайд назад
-function prevSlide() {
-  sliderCount++;
-  if (sliderCount < 0) sliderCount = sliderImages.lenght - 1;
-}
+const sliderLine = document.querySelector(".slider-line"),
+  sliderBtnPrev = document.querySelector(".slider-button__prev"),
+  sliderBtnNext = document.querySelector(".slider-button__next"),
+  sliderDots = document.querySelectorAll(".slider-pagination__button");
+let position = 0,
+  dotIndex = 0;
+// Functions
+const nextSlide = () => {
+  if (position < (sliderDots.length - 1) * 475) {
+    position += 475;
+    dotIndex++;
+  } else {
+    position = (sliderDots.length - 1) * 475;
+    dotIndex = 4;
+  }
+  sliderLine.style.left = -position + "px";
+  thisSlide(dotIndex);
+};
+const prevSlide = () => {
+  if (position > 0) {
+    position -= 475;
+    dotIndex--;
+  } else {
+    position = 0;
+    dotIndex = 0;
+  }
+  sliderLine.style.left = -position + "px";
+  thisSlide(dotIndex);
+};
+const thisSlide = (index) => {
+  for (let dot of sliderDots) {
+    dot.classList.remove("active");
+  }
+  sliderDots[index].classList.add("active");
+};
+// EVENTLISTENERS
+sliderBtnNext.addEventListener("click", nextSlide);
+sliderBtnPrev.addEventListener("click", prevSlide);
+
+sliderDots.forEach((dot, index) => {
+  dot.addEventListener("click", () => {
+    position = 475 * index;
+    sliderLine.style.left = -position + "px";
+    dotIndex = index;
+    thisSlide(dotIndex);
+  });
+});
