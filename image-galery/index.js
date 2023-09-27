a55e8c230c595cb4ca1da3be9f0a8a3cf74e3709
Shtepselevich <sinelnikov.romann@yandex.ru>
2023-09-27 23:04:38 +0300

    feat:created js

--- image-galery/index.js
+++ image-galery/index.js
@@ -1,8 +1,8 @@
 const input = document.getElementById("input");
-const grid = document.getElementsByClassName("grid");
+const grid = document.getElementsByClassName("grid")[0];
 
 input.addEventListener("keydown", function (event) {
-  if (event.key === "Enter" || event.key === "Click") loadImg();
+  if (event.key === "Enter") loadImg();
 });
 function loadImg() {
   removeImages();
@@ -11,4 +11,27 @@ function loadImg() {
     "https://api.unsplash.com/search/photos?query=" +
     input.value +
     "&per_page=9&client_id=1cfzsA17tpQI4OFEMGIxvNDbeP5uYoLRsYrN5sOMhxQ";
+
+  fetch(url)
+    .then((response) => {
+      if (response.ok) return response.json();
+      else alert(response.status);
+    })
+
+    .then((data) => {
+      const imageNodes = [];
+      for (let i = 0; i < data.results.lenghts; i++) {
+        imageNodes[i] = document.createElement("div");
+        imageNodes[i].className = "img";
+        imageNodes[i].style.backgroundImg =
+          "url(" + data.results[i].url.raw + ")";
+        imageNodes[i].addEventListener("dblclick", function () {
+          window.open(data.results[i].links.download, "_blank");
+        });
+        grid.appendChild(imageNodes[i]);
+      }
+    });
+}
+function removeImages() {
+  grid.innerHTML = "";
 }
