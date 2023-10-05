1ffa97e18f6a98bee32578f347ca307468e1f1a0
Shtepselevich <sinelnikov.romann@yandex.ru>
2023-10-05 15:47:01 +0300

    feat: created logic js

--- random-game/index.js
+++ random-game/index.js
@@ -4,6 +4,7 @@ window.addEventListener("load", function () {
   let userField = document.querySelector(".user-field");
   let compField = document.querySelector(".comp-field");
   let sound = document.querySelector(".sound");
+  let res = document.querySelector(".results");
   let play = document.querySelector(".play");
   let fields = document.querySelectorAll(".field");
   let userStep;
@@ -15,6 +16,10 @@ window.addEventListener("load", function () {
   function choiceUser(e) {
     if (blocked) return;
     let target = e.target;
+    // Если пользователь кликнул на изображение внутри кнопки
+    if (target.classList.contains("icon")) {
+      target = target.parentElement; // Получаем родительскую кнопку
+    }
     if (target.classList.contains("field")) {
       userStep = target.dataset.field;
       fields.forEach((item) => item.classList.remove("active", "error"));
@@ -26,9 +31,64 @@ window.addEventListener("load", function () {
     blocked = true;
     let rand = Math.floor(Math.random() * 3);
     compField.classList.add("blink");
+    let compFields = compField.querySelectorAll(".field");
+    setTimeout(() => {
+      compField.classList.remove("blink");
+      compStep = compFields[rand].dataset.field;
+      compFields[rand].classList.add("active");
+      winner();
+    }, 3000);
+    sound.setAttribute("src", "audio/choise.mp3");
+    sound.play();
   }
-  function winner(e) {}
-  function playGame() {}
+  function winner() {
+    blocked = false;
+
+    let comb = userStep + compStep;
+
+    switch (comb) {
+      case "rr":
+      case "ss":
+      case "pp":
+        res.innerText = "Draw!";
+        sound.setAttribute("src", "audio/organ.mp3");
+        sound.play();
+        break;
+
+      case "rs":
+      case "sp":
+      case "pr":
+        res.innerText = "You Win!";
+        sound.setAttribute("src", "audio/win.mp3");
+        sound.play();
+        countU++;
+        countUser.innerText = countU;
+        compField
+          .querySelector("[data-field=" + compStep + "]")
+          .classList.add("error");
+        break;
+
+      case "sr":
+      case "ps":
+      case "rp":
+        res.innerText = "You Lose!";
+        sound.setAttribute("src", "audio/lose.mp3");
+        sound.play();
+        countC++;
+        countComp.innerText = countC;
+        userField
+          .querySelector("[data-field=" + userStep + "]")
+          .classList.add("error");
+        break;
+    }
+  }
+  function playGame() {
+    countU = countC = 0;
+    res.innerText = "Make Choise!";
+    countUser.innerText = "0";
+    countComp.innerText = "0";
+    fields.forEach((item) => item.classList.remove("active", "error"));
+  }
 
   play.addEventListener("click", playGame);
   userField.addEventListener("click", choiceUser);
